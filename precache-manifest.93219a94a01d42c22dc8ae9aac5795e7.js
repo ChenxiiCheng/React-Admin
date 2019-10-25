becd3d17054ed5c07f0c6fb311666596206a2a78
ChenxiiCheng <chenxic1011@gmail.com>
2019-10-25 14:52:39 -0400

    Updates

---
+++ precache-manifest.93219a94a01d42c22dc8ae9aac5795e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430a248ea2105ef75b7f8efd7e75b481",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1492d6a5ba70f8cc1d68",
+    "url": "./static/css/2.31834a01.chunk.css"
+  },
+  {
+    "revision": "18eba4723024b8859eab",
+    "url": "./static/css/main.6070ec5f.chunk.css"
+  },
+  {
+    "revision": "1492d6a5ba70f8cc1d68",
+    "url": "./static/js/2.a56d71f4.chunk.js"
+  },
+  {
+    "revision": "18eba4723024b8859eab",
+    "url": "./static/js/main.aec1454e.chunk.js"
+  },
+  {
+    "revision": "c212b3cdc99850f7e8ed",
+    "url": "./static/js/runtime~main.571ebc19.js"
+  }
+]);
